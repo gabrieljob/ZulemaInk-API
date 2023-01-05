b7431cbbf24caf80cb2682890a129f60467eca74
Gabriel Linck <gabriel.jobc13@gmail.com>
2023-01-05 14:29:15 -0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -7,22 +7,38 @@ import auth from "./routes/auth";
 import root from "./routes/root";
 import { isAuthenticated } from "./middlewares/authentication";
 
-AppDataSource.initialize()
-  .then(() => {
-    const app = express();
-    app.use(cors());
-    app.use(express.json());
-    app.use(
-      bodyParser.urlencoded({
-        extended: true,
-      })
-    );
-    app.use("/api/v1/budget", isAuthenticated, budget);
-    app.use("/api/v1/auth", auth);
-    app.use("/api/v1/", root);
+// AppDataSource.initialize()
+//   .then(() => {
+//     const app = express();
+//     app.use(cors());
+//     app.use(express.json());
+//     app.use(
+//       bodyParser.urlencoded({
+//         extended: true,
+//       })
+//     );
+//     app.use("/api/v1/budget", isAuthenticated, budget);
+//     app.use("/api/v1/auth", auth);
+//     app.use("/api/v1/", root);
 
-    app.listen(3333, () => {
-      console.log("init server");
-    });
+//     app.listen(3333, () => {
+//       console.log("init server");
+//     });
+//   })
+//   .catch((error) => console.log(error));
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
   })
-  .catch((error) => console.log(error));
+);
+// app.use("/api/v1/budget", isAuthenticated, budget);
+// app.use("/api/v1/auth", auth);
+app.use("/api/v1/", root);
+
+app.listen(3333, () => {
+  console.log("init server");
+});
